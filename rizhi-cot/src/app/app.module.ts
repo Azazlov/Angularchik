da53c07b38e531c117f7b225d8203fc942566712
Azazlov <Azazlov mfd404mf@gmail.com>
2025-03-11 10:43:52 +0300

    Нууу ээээ готово

--- rizhi-cot/src/app/app.module.ts
+++ rizhi-cot/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { PagesComponent } from './pages/pages.component';
 import { HeaderComponent } from './@theme/components/header/header.component';
 import { SidebarComponent } from './@theme/components/sidebar/sidebar.component';
 import { FooterComponent } from './@theme/components/footer/footer.component';
+import { MenuParentsComponent } from './@theme/components/menu/components/menu-parents/menu-parents.component';
+import { MenuChildrensComponent } from './@theme/components/menu/components/menu-childrens/menu-childrens.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { FooterComponent } from './@theme/components/footer/footer.component';
     PagesComponent,
     HeaderComponent,
     SidebarComponent,
-    FooterComponent
+    FooterComponent,
+    MenuParentsComponent,
+    MenuChildrensComponent
   ],
   imports: [
     BrowserModule,
